54da584f6de0749737bf6ecae6ccd430b7fd5344
user <user@Keyonnas-MacBook-Air.local>
2018-11-18 13:34:42 -0500

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://thedeen:salaam1@ds035907.mlab.com:35907/thedeen' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
